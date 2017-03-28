113d94a09b8fb5a72ad1f1435d415b8aacd7f72b
charles agyemang <micnkru@gmail.com>
2017-03-28 03:55:46 +0000

    added a folder called v1 and namespaced my routes to rtake the form of the v1 i also modularized my controller to inherit form the folder i created accordting to the rails architecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :people
+
+    namespace :v1 do
+        resources :people do
+          resources :posts
+        end
+        resources :posts
+   end
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
