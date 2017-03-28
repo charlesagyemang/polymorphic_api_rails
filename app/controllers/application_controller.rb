7b518c538da4e2b046d357feb5903775bef24407
charles agyemang <micnkru@gmail.com>
2017-03-28 02:51:27 +0000

    Finale set up to get my app running. I added cors, checked rack attack, initalizes serialers in my application.rb and wrote a fuction that will render pretty json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+
+  #Include Serialization...
+  include ActionController::Serialization
+
+  #function to render pretter json
+  def render_pretty_json(params)
+    render json: JSON.pretty_generate(params.as_json)
+  end
+
 end
