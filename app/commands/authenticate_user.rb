e2bf83cb4151909d54ec15b4a9733c9e8c4bdd43
charles agyemang <micnkru@gmail.com>
2017-03-29 12:35:13 +0000

    hadnt saved some stuff but just did

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -4,7 +4,7 @@ class AuthenticateUser
   def initialize(email, password)
     @email = email
     @password = password
-  end 
+  end
 
   def call
     JsonWebToken.encode(user_id: user.id) if user
