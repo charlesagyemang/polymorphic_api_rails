7124d299b2293becc37108c8113970d9071f7547
charles agyemang <micnkru@gmail.com>
2017-03-29 12:28:22 +0000

    added a header t authenticate user before the application controller itself opens yh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::API
 
+  before_action :authenticate_request
+
+  attr_reader :current_user
+
+
   #Include Serialization...
   include ActionController::Serialization
 
@@ -9,15 +14,22 @@ class ApplicationController < ActionController::API
   end
 
   #Do this login in the front end
-  def getPostsById (id)
-    post_id = []
-    posts = Post.all
-    posts.each do |post|
-      if post.person_id == id
-        post_id.push(post)
-      end
-    end
-    post_id
-  end
+  # def getPostsById (id)
+  #   post_id = []
+  #   posts = Post.all
+  #   posts.each do |post|
+  #     if post.person_id == id
+  #       post_id.push(post)
+  #     end
+  #   end
+  #   post_id
+  # end
 
+
+   private
+
+   def authenticate_request
+     @current_user = AuthorizeApiRequest.call(request.headers).result
+     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
+   end
 end
